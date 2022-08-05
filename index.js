06fb857e64d42cb0df4ee675146b1923d84ef0e2
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-08-05 21:55:11 +0530

    code formatted

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 const express = require('express');
-const app=express();
+const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-//Route imports
+// Route imports
 const authRoute = require('./routes/auth');
 // const postRoute= require('./routes/posts');
 
 dotenv.config();
 
 //mongo connect
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true},()=>{console.log("Mongo Connection Success");});
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => { console.log("Mongo Connection Success"); });
 
 
-
-//global middlewares
-
+// Global middlewares
 app.use(express.json());
 
-//route middleware
-app.use('/auth',authRoute);
+// Route middleware
+app.use('/auth', authRoute);
 
-app.get('/',(req,res)=> {
+app.get('/', (req, res) => {
     res.send("Hoiiii")
 });
 
 
-
-
-
-app.listen(parseInt(process.env.PORT),()=>{console.log("The server is listening on 6905");});
-
+app.listen(parseInt(process.env.PORT), () => { console.log("The server is listening on 6905"); });
