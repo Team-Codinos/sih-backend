714d239001c41aae1fb9828851a6c2538f4e83b0
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-08-05 22:59:21 +0530

    response codes added

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,31 @@
 const router = require("express").Router();
-const { exists } = require("../model/User");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require("../model/User");
 const validate = require("../validation"); //your exports can be like this OR
-// const {loginvalidation,registervalidation} =require('../validation'); //like this as you can only take required ones from the exports
+// const {loginvalidation,registervalidation} = require('../validation'); //like this as you can only take required ones from the exports
 //  in order you want and the names you want
 
 router.post("/register", async (req, res) => {
   //validate beofre adding the user
   const { error } = validate.registerValidation(req.body); //left hand said is also called destruction btw
-  
+
   if (error)
-   return res.status(400).json({ "error": error.details[0].message });
+    return res.status(400).json({
+      auth: false,
+      id: null,
+      errMsg: error.details[0].message
+    });
 
   //once you make sure everything is valid check if the user already exists
   const emailExist = await User.findOne({ email: req.body.email });
-  
+
   if (emailExist)
-   return res.status(400).send("Email Already exists");
+    return res.status(403).json({
+      auth: false,
+      id: null,
+      errMsg: "E-mail already exists"
+    });
 
 
   //hash the password before giving it to the db
@@ -34,35 +41,62 @@ router.post("/register", async (req, res) => {
 
   try {
     const savedUser = await user.save();
-    res.send(savedUser._id);
+    res.status(200).json({
+      auth: true,
+      id: savedUser._id,
+      errMsg: null
+    });
+
   } catch (error) {
     console.log(error);
-    res.status(401).json({ "error": error.toString() });
+
+    res.status(401).json({
+      auth: false,
+      id: null,
+      errMsg: error.toString()
+    });
   }
 });
 
 router.post("/login", async (req, res) => {
   //validate beofre adding the user
   const { error } = validate.loginValidation(req.body); //left hand said is also called destruction btw
-  
+
   if (error)
-   return res.status(400).json({ "error": error.details[0].message });
+    return res.status(400).json({
+      auth: false,
+      id: null,
+      errMsg: error.details[0].message
+    });
 
   //once you make sure everything is valid check if the user exists
   const currentuser = await User.findOne({ email: req.body.email });
-  
+
   if (!currentuser)
-   return res.status(400).send("Email or Password is wrong pwd");
+    return res.status(404).json({
+      auth: false,
+      id: null,
+      errMsg: "User not found!"
+    });
 
   //check if password is corrent
   const pwdValid = await bcrypt.compare(req.body.password, currentuser.password);
-  
+
   if (!pwdValid)
-   return res.status(400).send("Email or Password Incorrect");
+    return res.status(401).json({
+      auth: false,
+      id: null,
+      errMsg: "Email or Password is Incorrect"
+    });
 
   //create and assign a token
   const token = jwt.sign({ "id": currentuser._id }, process.env.TOKEN_SECRET);
-  res.header('auth-token', token).send(token);
+
+  res.status(200).header('auth-token', token).json({
+    auth: true,
+    id: currentuser._id,
+    errMsg: null
+  });
 });
 
 module.exports = router;
