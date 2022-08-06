03473ca6545a2ce6f2709748ae47bcc03674cb1f
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-08-06 16:43:40 +0530

    primary-enrollment route added

--- routes/data.js
+++ routes/data.js
@@ -3,16 +3,16 @@ const router = require("express").Router();
 //ROUTE IMPORTS
 const passFailRoute = require('./data/pass-fail');
 const literacyRoute = require('./data/literacy');
-const dropoutRouter = require('./data/dropouts_route');
-const primaryEnrollmentRouter = require('./data/primary_enrollment');
+const dropoutRoute = require('./data/dropouts_route');
+const primaryEnrollmentRoute = require('./data/primary_enrollment');
 
 
 
 //ASSIGNING APPROPRIATE ROUTES
 router.use("/pass-fail", passFailRoute);
 router.use("/literacy-rate", literacyRoute);
-router.use("/dropouts", dropoutRouter);
-router.use("/primary-enrollment", primaryEnrollmentRouter);
+router.use("/dropouts", dropoutRoute);
+router.use("/primary-enrollment", primaryEnrollmentRoute);
 
 
 module.exports = router;
