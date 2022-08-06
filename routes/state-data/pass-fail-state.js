50f4b937febdb9fcee308eb06c9a2d80b7ed091d
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-08-06 18:58:36 +0530

    new routes added

---
+++ routes/state-data/pass-fail-state.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const state_wise_pass_fail = require("../../model/pass-fail/state_wise_pass_fail_rates");
+
+router.post('/', async (req, res) => {
+    const year = req.body.year || 2000;
+
+    try {
+        const queryResult = await state_wise_pass_fail.find({ year: year });
+        const resultMap = {};
+
+        queryResult.forEach((obj) => {
+            resultMap[obj.State] = {
+                boys: obj.BOYS,
+                girls: obj.GIRLS
+            };
+        });
+
+        return res.status(200).json(resultMap);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Database error" })
+    }
+});
+
+module.exports = router;
